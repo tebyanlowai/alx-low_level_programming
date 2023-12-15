14bb5d988fd22d14af0ed1171d65ebb6691d0249
tebyanlowai <tebyan@DESKTOP-UCMOB9L.localdomain>
2023-12-15 19:57:19 +0200

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
  * description: A C Program That Print With puts Function
  * return: Alawys 0 (ASuccess)
  * main: Entery - point
 */
-
 int main(void)
 {
-	 puts("\"Programming is like building a multilingual puzzle");	
-         return (0); 
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
