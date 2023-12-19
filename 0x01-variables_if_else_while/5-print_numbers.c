9ced83987d2a244a1a54cee0da9d9ae5a82c2842
tebyanlowai <tebyan@DESKTOP-UCMOB9L.localdomain>
2023-12-19 14:22:45 +0200

    print _numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entery Point
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+pritf("%i", digit);
+digit++;
+}
+printf("\n");
+return (0);
+}
