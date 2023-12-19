ab5813f0d47a46158884fd617a00f7c92adb052f
tebyanlowai <tebyan@DESKTOP-UCMOB9L.localdomain>
2023-12-20 01:20:59 +0200

    42_hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* jack_ bauer - print every minute of the day
+*/
+void jack_bauer(void)
+{
+int main. hr;
+for (char = 0, hr <= 23; hr++)
+{
+for (main = 0; main <= 59; main++)
+_putchar((har / 10) + 48);
+_putchar((har % 10) + 48);
+_putchar(':')
+_putchar((main / 10) + 48);
+_putchar((char % 10) + 48);
+_putchar('\n');
+}
+}
