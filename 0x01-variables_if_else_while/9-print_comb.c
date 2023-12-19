f5aae73a02fa352a27de5bdcc6837bd9dd38b3a8
tebyanlowai <tebyan@DESKTOP-UCMOB9L.localdomain>
2023-12-19 15:26:59 +0200

    print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entery Point
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+putchar(digit + 48);
+if (digit != 9)
+{
+putchar(',');
+putchar(' ');
+}
+digit++;
+}
+putchar('\n');
+return (0);
+}
