32d31c77b2d762cc09869cf0544761a2f616e48c
tebyanlowai <tebyan@DESKTOP-UCMOB9L.localdomain>
2023-12-19 14:13:31 +0200

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entery Point
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+if (ch == 'e' || ch == 'q')
+ch++;
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
