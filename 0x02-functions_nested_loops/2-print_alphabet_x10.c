49f8195ae22b8dde3b147dd6fd94f570109c9cfd
tebyanlowai <tebyan@DESKTOP-UCMOB9L.localdomain>
2023-12-19 23:40:07 +0200

    Alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 /**
  * print_alphabet_x10 - function that will print the alphabet 10 times
 */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 int line, ch;
 for (line = 0; line <= 9; line++)
