8130c2ca39662b3c7bc07d9721734b97fa28d970
tebyanlowai <tebyan@DESKTOP-UCMOB9L.localdomain>
2023-12-19 14:35:02 +0200

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entery Point
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+putchar(digit + '0');
+digit++;
+}
+printf("\n");
+return (0);
+}
