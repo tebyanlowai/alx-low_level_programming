c141c507dd3a08fca922e2b792830984d9c35677
tebyanlowai <tebyan@DESKTOP-UCMOB9L.localdomain>
2023-12-20 01:29:56 +0200

    42_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,13 +5,13 @@
 void jack_bauer(void)
 {
 int min, hr;
-for (hr = 0, hr <= 23; hr++)
+for (hr = 0; hr <= 23; hr++)
 {
 for (min = 0; min <= 59; min++)
 {
 _putchar((hr / 10) + 48);
 _putchar((hr % 10) + 48);
-_putchar(':')
+_putchar(':');
 _putchar((min / 10) + 48);
 _putchar((min % 10) + 48);
 _putchar('\n');
