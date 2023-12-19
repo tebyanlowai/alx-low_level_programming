7d7e39fb4ee44700c89beb63148c0c180aab4454
tebyanlowai <tebyan@DESKTOP-UCMOB9L.localdomain>
2023-12-20 01:22:37 +0200

    42_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,11 +8,11 @@ int main. hr;
 for (char = 0, hr <= 23; hr++)
 {
 for (main = 0; main <= 59; main++)
-_putchar((har / 10) + 48);
-_putchar((har % 10) + 48);
+_putchar((hr / 10) + 48);
+_putchar((hr % 10) + 48);
 _putchar(':')
 _putchar((main / 10) + 48);
-_putchar((char % 10) + 48);
+_putchar((main % 10) + 48);
 _putchar('\n');
 }
 }
