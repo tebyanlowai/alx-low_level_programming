c3ba456a0a40a31415221d86ca67ec12d3846eab
tebyanlowai <tebyan@DESKTOP-UCMOB9L.localdomain>
2023-12-19 13:59:21 +0200

    print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entery Point
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch = 'a';
+char CH = 'A';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+while (CH <= 'Z')
+{
+putchar(CH);
+CH++;
+}
+putchar('\n');
+return (0);
+}
