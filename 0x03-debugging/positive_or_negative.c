1ad8d68dc2a14fe535c63565d372d990c75fc363
tebyanlowai <tebyan@DESKTOP-UCMOB9L.localdomain>
2023-12-22 17:41:50 +0200

    Multiple_main

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,16 +1,14 @@
 #include <stdlib.h>
 #include "main.h"
-/**
- * main - Entery Point
- * Return: Always 0 (Success)
-*/
-void postitive_or_negative(int i)
+
+void positive_or_negative(int i)
 {
 if (i > 0)
-printf("%i is positive\n", i);
+{
+    printf("%i is positive\n", i);
+}
 else if (i == 0)
-printf("%i is zero\n", i);
+    printf("%i is zero\n", i);
 else
-printf("%i is negative\n", i);
-return (0);
+    printf("%i is negative\n", i);
 }
