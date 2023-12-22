3548bb1205b33c28c5ddf9bdc4e41d87c3d01c66
tebyanlowai <tebyan@DESKTOP-UCMOB9L.localdomain>
2023-12-22 22:25:37 +0200

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _isupper - check if c is upper
- *
- * @c: input for alphabet
- *
- * Return: 1 if its uppercase, 0 if not 
+* _isupper - check if c is upper
+*
+* @c: input for alphabet
+*
+* Return: 1 if its uppercase, 0 if not
 */
 
 int _isupper(int c)
