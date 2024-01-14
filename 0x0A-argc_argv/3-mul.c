1eced54816b7614172320061a854a80c3a81e6ac
tebyanlowai <tebyan@DESKTOP-UCMOB9L.localdomain>
2024-01-14 22:49:42 +0200

    argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,17 +10,11 @@
 
 int main(int argc, char *argv[])
 {
-int sum;
-
-if (argc == 3)
-{
-sum = atoi(argv[1] * atoi(argv[2]));
-printf("%d\n", sum);
-}
-else
+if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
+printf("%d\n", atoi (argv[1]) * atoi(argv[2]));
 return (0);
 }
