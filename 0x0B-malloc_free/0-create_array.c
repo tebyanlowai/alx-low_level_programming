797014ceb5552b3bf5645b1bfa173e915f6f630f
tebyanlowai <tebyan@DESKTOP-UCMOB9L.localdomain>
2024-01-19 15:07:09 +0200

    malloc,free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@
 
 char *create_array(unsigned int size, char c)
 {
-char = *m = malloc(size);
+char *m = malloc(size);
 if (size == 0 || m == 0)
 return (0);
 
@@ -18,5 +18,4 @@ while (size--)
 m[size] = c;
 
 return (m);
-
 }
