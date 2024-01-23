158f0c85c7d29a42742ed790c1404f2b6ae687fb
tebyanlowai <tebyan@DESKTOP-UCMOB9L.localdomain>
2024-01-23 17:13:17 +0200

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - A dog struct
+* @name: dog name
+* @age: dog age
+* @owner: owner name
+*
+* Description: dog struct in bitty world
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
